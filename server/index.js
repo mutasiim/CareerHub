290f1dc8716e6acbb1cf14ee599a0e2b8cf097ac
Md. Mutasim Chowdhury <md.mutasimchowdhury@Mds-MacBook-Air.local>
2026-08-06 09:42:04 -0500

    Fix Greenhouse job description formatting

--- server/index.js
+++ server/index.js
@@ -383,20 +383,37 @@ function normalizeLeverJob(job, site, searchQuery = "") {
   };
 }
 
+function decodeHtmlEntities(value = "") {
+  let decoded = asCleanString(value);
+
+  // Some ATS providers return descriptions with HTML encoded more than once.
+  for (let pass = 0; pass < 2; pass += 1) {
+    decoded = decoded
+      .replace(/&amp;/gi, "&")
+      .replace(/&lt;/gi, "<")
+      .replace(/&gt;/gi, ">")
+      .replace(/&quot;/gi, '"')
+      .replace(/&#39;|&apos;/gi, "'")
+      .replace(/&nbsp;/gi, " ")
+      .replace(/&#x([0-9a-f]+);/gi, (_, hex) =>
+        String.fromCodePoint(Number.parseInt(hex, 16)),
+      )
+      .replace(/&#(\d+);/g, (_, decimal) =>
+        String.fromCodePoint(Number.parseInt(decimal, 10)),
+      );
+  }
+
+  return decoded;
+}
+
 function stripHtml(value = "") {
-  return asCleanString(value)
+  return decodeHtmlEntities(value)
     .replace(/<br\s*\/?\s*>/gi, "\n")
     .replace(/<li\b[^>]*>/gi, "\n• ")
     .replace(/<\/li>/gi, "")
     .replace(/<\/(?:p|div|section|article|h[1-6]|ul|ol)>/gi, "\n\n")
     .replace(/<(?:p|div|section|article|h[1-6]|ul|ol)\b[^>]*>/gi, "")
     .replace(/<[^>]*>/g, "")
-    .replace(/&nbsp;/gi, " ")
-    .replace(/&amp;/gi, "&")
-    .replace(/&quot;/gi, '"')
-    .replace(/&#39;/gi, "'")
-    .replace(/&lt;/gi, "<")
-    .replace(/&gt;/gi, ">")
     .replace(/[ \t]+/g, " ")
     .replace(/ *\n */g, "\n")
     .replace(/\n{3,}/g, "\n\n")
